ed97984d376fa71f90137e94950d87840a5b4a2e
denim02 <denimastori02@gmail.com>
2023-01-19 01:02:02 +0100

    chore: fix coverage CI

--- tests.py
+++ tests.py
@@ -1,5 +1,15 @@
 import unittest
+import pieces
 
 class Test(unittest.TestCase):
     def test(self):
         self.assertEqual(1, 1)
+
+class TestPieces(unittest.TestCase):
+    def test_piece_attributes(self):
+        piece = pieces.Pawn("white", (0, 0))
+        self.assertEqual(piece.name, "Pawn")
+        self.assertEqual(piece.value, 1)
+        self.assertEqual(piece.color, "white")
+        self.assertEqual(piece.position, (0, 0))
+        self.assertEqual(piece.legal_moves, set())
