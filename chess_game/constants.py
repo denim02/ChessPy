8681419c539eb06231b3e4ec56b1324998d557d0
denim02 <denimastori02@gmail.com>
2023-03-04 15:22:59 +0200

    refactor: setup new constants module and remove former .ini file

---
+++ chess_game/constants.py
@@ -0,0 +1,15 @@
+import pygame
+
+# Window constants
+WINDOW_WIDTH, WINDOW_HEIGHT = 720, 720
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WINDOW_WIDTH // COLS 
+
+# Color palette
+DARK = "#769656"
+LIGHT = "#EEEEE2"
+
+# Game settings 
+STARTING_FEN_FILE = "./game_states/init_position.fen"
+MOVE_LOG_ENABLED = True
+MOVE_LOG_FILE = "./game/logs/move_log.txt"
